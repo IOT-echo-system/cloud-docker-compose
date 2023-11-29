ba32b04363971c8fe11d87ceca1be46b0891b60d
Shivam Rajput <shivam.rajput@thoughtworks.com>
2023-11-29 08:13:25 +0530

    shivi | update init.sh with mosquitto config.

--- init.sh
+++ init.sh
@@ -2,3 +2,13 @@
 
 mkdir -p ./mqtt/data ./mqtt/config ./mqtt/log
 touch ./mqtt/config/mosquitto.conf
+sudo echo 'persistence true
+persistence_location /mosquitto/data
+user mosquitto
+listener 1883
+allow_anonymous false
+log_dest stdout
+log_dest file /mosquitto/log/mosquitto.log
+connection_messages true
+password_file /mosquitto/config/pwfile' >> ./mqtt/config/mosquitto.conf
+sudo touch ./mqtt/config/pwfile
