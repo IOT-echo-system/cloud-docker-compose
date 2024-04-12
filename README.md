8084ac9b0e6cf0004e57aba265484cee8bedb99a
Shivam Rajput <shivam.rajput@thoughtworks.com>
2024-04-12 07:01:28 +0530

    Shivi | update README.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Robotutor Tech IOT Plateform
 
 ## Steps to start
--
+- start `sudo sh init.sh`
+- create user in backend-mqtt
+- `sudo docker exec -it backend-mqtt sh`
+- `mosquitto_passwd -c /mosquitto/config/pwfile username`
+- exit and restart the container
+- create users in node-mqtt
+- `sudo docker exec -it node-mqtt sh`
+- `mosquitto_passwd -c /mosquitto/config/pwfile username` create 2 users board and node-bff
+define the entries in acl_file.conf
+```
+user user-name
+topic read/topic-name-with-wild-card  
+topic write/topic-name-with-wild-card  
+```
--- init.sh
+++ init.sh
@@ -16,18 +16,17 @@ password_file /mosquitto/config/pwfile' > ./backend-mqtt/config/mosquitto.conf
 mkdir -p ./node-mqtt/data ./node-mqtt/config ./node-mqtt/log
 touch ./node-mqtt/config/mosquitto.conf
 touch ./node-mqtt/config/acl_file.conf
+touch ./node-mqtt/config/pwfile
 sudo echo 'persistence true
 persistence_location /mosquitto/data
 user mosquitto
 listener 1883
 allow_anonymous false
-per_listener_settings false
-plugin /usr/lib/mosquitto_dynamic_security.so
-plugin_opt_config_file /mosquitto/config/dynamic-security.json
 log_dest stdout
 log_dest file /mosquitto/log/mosquitto.log
 connection_messages true
-acl_file /mosquitto/config/acl_file.conf' > ./node-mqtt/config/mosquitto.conf
+acl_file /mosquitto/config/acl_file.conf
+password_file /mosquitto/config/pwfile' > ./node-mqtt/config/mosquitto.conf
 
 mkdir -p mongo-data
 sudo chown -R $USER mongo-data
